65d00c53a6ea42b5386e85c84d45bed867e53245
Gabriel Felipe Freire Silva <gabeielfelipe892@gmail.com>
2026-03-04 19:52:14 -0300

    Add files via upload

---
+++ bgst5/static/js/inst.js
@@ -0,0 +1,21 @@
+
+function abrirModal(nome, cargo, descricao, urlFoto) {
+    document.getElementById('modal-nome').innerText = nome;
+    document.getElementById('modal-cargo').innerText = cargo;
+    document.getElementById('modal-desc').innerText = descricao;
+    document.getElementById('modal-foto').src = urlFoto;
+
+    document.getElementById('modal-equipe').style.display = 'flex';
+}
+
+
+function fecharModal() {
+    document.getElementById('modal-equipe').style.display = 'none';
+}
+
+window.onclick = function(event) {
+    let modal = document.getElementById('modal-equipe');
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
